33c65096ceb6849b5c649ad9a4c9b8d762e332a6
fjhickernell <hickernell@iit.edu>
2013-07-29 23:05:14 -0500

    little changes in .m

--- ConesKernelsError/KernelConesGuarantees.m
+++ ConesKernelsError/KernelConesGuarantees.m
@@ -1,21 +1,25 @@
 %Cones and Kernel Approximation for Guarantees
+%% Preliminaries
 clear all
 close all
 format compact
 set(0,'defaultaxesfontsize',20,'defaulttextfontsize',20)
 testfun=@(x) sin(8*x);
 
-a=10;
+%% Kernel
+a=20;
 kernel=@(x,t) exp(-(a.^2)*(bsxfun(@minus,x,t')).^2);
 
-n=20;
+%% Data and spline approximation
+n=25;
 xnode=linspace(0,1,n)';
 Kmat=kernel(xnode,xnode);
-%xnode=0.5;
+condK=cond(Kmat)
 y=testfun(xnode);
 c=Kmat\y;
 splinef=@(x) kernel(x,xnode)*c;
 
+%% Plot of test function and spline
 nplot=500;
 xplot=linspace(0,1,nplot)';
 plot(xplot,testfun(xplot),'b-',...
@@ -24,13 +28,15 @@ plot(xplot,testfun(xplot),'b-',...
     'linewidth',3)
 ntest=1000;
 xtest=linspace(0,1,ntest)';
+
+%% Root mean square error
 error=sqrt(mean((testfun(xtest)-splinef(xtest)).^2))
 
 normHsplinef=c'*y
 xpxt=bsxfun(@plus,xnode,xnode');
-invKmat=inv(Kmat);
 Ktildemat=(sqrt(pi/2)/(2*a)) ...
     *exp(-((a^2)/2)*(bsxfun(@minus,xnode,xnode').^2)) ...
     .*(-erf((a/sqrt(2))*(xpxt-2)) + erf((a/sqrt(2))*xpxt));
-Herrbd=1-sum(sum(Ktildemat'.*invKmat,2))
+condKtilde=cond(Ktildemat)
+Herrbd=1-trace(Ktildemat/Kmat)
 guesserrest=Herrbd*normHsplinef
