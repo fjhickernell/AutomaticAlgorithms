52e75e3f38fdb4549d77d42f8ac1b25b698754e0
Xuan Zhou <xzhou23@hawk.iit.edu>
2013-11-17 15:36:47 -0600

    minor change

--- ConesKernelsError/KernelConesGuaranteesEmbedding.m
+++ ConesKernelsError/KernelConesGuaranteesEmbedding.m
@@ -4,17 +4,18 @@ clear all
 close all
 format compact
 set(0,'defaultaxesfontsize',20,'defaulttextfontsize',20)
-testfun=@(x) sin(2*pi*x);
+a=3;
+testfun=@(x) exp(a*x)-1;
 tau=pi;
 epsilon = 1e-2;
 
 %% Kernel
-a=1;
-kernel=@(x,t) a*bsxfun(@min,x,t');
+gamma=1;
+kernel=@(x,t) gamma*bsxfun(@min,x,t');
 
 %% Data and spline approximation
-n=15;
-xnode=linspace(0.01,.99,n)';
+n=100
+xnode=linspace(1/n,1,n)';
 Kmat=kernel(xnode,xnode);
 condK=cond(Kmat)
 y=testfun(xnode);
@@ -28,20 +29,22 @@ plot(xplot,testfun(xplot),'b-',...
     xplot,splinef(xplot),'r--',...
     xnode,y,'k.','markersize',20,...
     'linewidth',3)
-ntest=1000;
+ntest=1007;
 xtest=linspace(0,1,ntest)';
 
 %% Root mean square error
-error=sqrt(mean((testfun(xtest)-splinef(xtest)).^2))
-normHsplinef=sqrt(c'*y)
+error2=sqrt(mean((testfun(xtest)-splinef(xtest)).^2))
+error_sup=max(abs(testfun(xtest)-splinef(xtest)))
+%normHsplinef=sqrt(c'*y)
 xtPlus=bsxfun(@plus,xnode,xnode');
 xtTimes=bsxfun(@times,xnode,xnode');
 xtMin=bsxfun(@min,xnode,xnode');
 xtMax=bsxfun(@max,xnode,xnode');
-Ktildemat = a^2*(xtMin.^3/3+xtMin.*(xtMax.^2-xtMin.^2)/2+xtTimes.*(1-xtMax));
+Ktildemat = gamma^2*(xtMin.^3/3+xtMin.*(xtMax.^2-xtMin.^2)/2+xtTimes.*(1-xtMax));
 condKtilde=cond(Ktildemat)
-Herrbd=sqrt(a/2-trace(Ktildemat/Kmat))
-guesserrest=Herrbd*normHsplinef
+traceKK=trace(Ktildemat/Kmat)
+Herrbd=sqrt(gamma/2-traceKK)
+%guesserrest=Herrbd*normHsplinef
 
 %% Algorithm 1 Stage 1
 normTsplinef=sqrt(c'*Ktildemat*c);
