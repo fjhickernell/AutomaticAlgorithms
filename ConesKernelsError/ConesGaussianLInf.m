52853c690a19e02c585d01d59a3422a99a2a382f
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-08-25 16:07:43 -0500

    add order 3 estimate

--- ConesKernelsError/ConesGaussianLInf.m
+++ ConesKernelsError/ConesGaussianLInf.m
@@ -4,19 +4,20 @@ clear all
 close all
 format compact
 set(0,'defaultaxesfontsize',20,'defaulttextfontsize',20)
-n = 100
+n = 300
 a = 0; b = 1;
 xnode=linspace(a,b,n)';
 h = (b-a)/(2*n); R = (b-a)/2;
 testfun=@(x) sin(pi*x);
 gamma = 1;
 kernel=@(x,t) exp(-gamma*(bsxfun(@minus,x,t')).^2);
-tau = 2;
+tau = 4;
 %c0 = R/6; C1 = 2304*gamma; C2 = log(4)+16; C3 = C1*exp(C2);
 %C = min(c0/2,1/(exp(1)*C3))/2;
 %C = min(c0/2,1/sqrt(exp(1)*C3))/2;
 C4 = 64/3; %C5 = 1/2; c1 = sqrt(2*gamma^2*C5*C4^3*(6+C5*C4));
-C = 3*sqrt(2)*gamma*C4^2;
+C = 4*gamma*C4^2;
+CC = sqrt(8)*gamma^(3/2)*C4^3;
 h0 = min([1/sqrt(tau*C) R/C4 sqrt(3)/C4/sqrt(2*gamma)]);
 %epsilon = 1e-2;
 
@@ -47,6 +48,7 @@ normHsplinef=sqrt(c'*y)
 %eBoundEst = -tau*exp(C*log(h)/h)/(1-tau*c1*h)*fval
 [~,fval] = fminbnd(@(x) -abs(splinef(x)),a,b)
 eBoundEst = -tau*C*h^2*fval/(1-tau*C*h^2)
+eBoundEst2 = -tau*CC*h^3*fval/(1-tau*CC*h^3)
 
 
 
